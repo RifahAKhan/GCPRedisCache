cc11e9b498055c59a1b120d05cb815ff931a5c0c
Khan <rifkhan@apac.corpdir.net>
2024-09-24 16:20:24 +0530

    first commit

--- download_image.py
+++ download_image.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     client = get_redis_client(redis_host, redis_port)
 
     redis_key = 'image_key'  # Key under which image was stored
-    output_path = 'C:\Users\RIFKHAN\Desktop\GCPRedisCache\destination'  # Ensure this directory exists
+    output_path = '/home/rifahakhan/GCPRedisCache/destination/gcp_downloaded.png'  # Ensure this directory exists
   # Destination folder
 
     start_time = time.time()
